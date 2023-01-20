b1f73cd2c353031a352d613f9ac3583ba26c3f81
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-20 21:31:19 +0600

    created meal component

---
+++ src/components/Header/Meal/Meal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Meal = () => {
+    return (
+        <div className='meal-container'>
+            <div className="item-container">
+                <h2>Items</h2>
+            </div>
+            <div className="details-container">
+                <h2>Details</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Meal;
