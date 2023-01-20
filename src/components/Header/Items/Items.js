86857a89c81cd5ff80b022f53e6a10a6e1a86edf
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-20 21:44:04 +0600

    created item component

---
+++ src/components/Header/Items/Items.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Items = () => {
+    return (
+        <div>
+            <h3>This is item</h3>
+        </div>
+    );
+};
+
+export default Items;
--- src/components/Header/Meal/Meal.js
+++ src/components/Header/Meal/Meal.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import './Meal.css'
 
 const Meal = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+            .then(res => res.json())
+            .then(data => setItems(data.meals))
+    }, []);
+
     return (
         <div className='meal-container'>
             <div className="item-container">
-                <h2>Items</h2>
+                <h2>Items: {items.length}</h2>
             </div>
             <div className="details-container">
                 <h2>Details</h2>
